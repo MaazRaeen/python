eea090b0ced14656476d6e8c8eb633ce9829628d
MaazRaeen <Maazraeen42@gmail.com>
2025-08-09 12:18:24 +0530

    changed qr color

--- extra.py
+++ extra.py
@@ -5,8 +5,8 @@ qr = qrcode.QRCode(box_size=10, border=4)
 qr.add_data(data)
 qr.make(fit=True)
 
-img = qr.make_image(fill_color="black", back_color="white")
-img.save("qrcode.png")
+img = qr.make_image(fill_color="black", back_color="yellow")
+img.save("./downloads+qrcode.png")
 
 print("QR Code saved as qrcode.png")
 import os
